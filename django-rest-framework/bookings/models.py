17753598f624f937e8c48cf22e67d3a3d644c307
Cotbert <mateojgdev@gmail.com>
2024-10-26 09:39:02 -0500

    booking model

--- django-rest-framework/bookings/models.py
+++ django-rest-framework/bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from patients.models import Patient
+from doctors.models import Doctor
 
 # Create your models here.
+
+class Appoitment(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(max_length=100)
+
+class MedicalNote(models.Model):
+    id = models.AutoField(primary_key=True)
+    note = models.TextField()
+    date = models.DateField()
